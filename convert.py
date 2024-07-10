a6978ce33033d39275b148123fe6e869f2da4258
Magdalena-N <magdusiamn@gmail.com>
2024-07-10 12:04:34 +0200

    Adapt convert.py script to own purposes. Update .gitignore.

--- convert.py
+++ convert.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 MODELS_DIR = "saved_models"
 OUTPUT_DIR = "converted_models"
 
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     filename='convert.log',
@@ -17,10 +16,11 @@ console = logging.StreamHandler()
 logger = logging.getLogger("convert")
 logger.addHandler(console)
 
-
 def convert(model_dir, save_dir):
     converter = tf.lite.TFLiteConverter.from_saved_model(model_dir)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converted_tflite_model = converter.convert()
+    os.makedirs(os.path.dirname(save_dir), exist_ok=True)
     open(save_dir, "wb").write(converted_tflite_model)
 
 
@@ -33,7 +33,8 @@ if __name__ == "__main__":
                 continue
             logger.info(f"Converting {model} to TFLite")
             try:
-                convert(os.path.join(MODELS_DIR, ver, model), os.path.join(OUTPUT_DIR, ver, model.replace("_frozen", ".tflite")))
+                convert(os.path.join(MODELS_DIR, ver, model),
+                        os.path.join(OUTPUT_DIR, ver, model.replace("_frozen", "_quant.tflite")))
                 logger.info(f"Successfully converted model {model} to TFLite")
             except Exception as e:
                 logger.error(f"Couldn't convert model {model} with exception: {str(e)}")
